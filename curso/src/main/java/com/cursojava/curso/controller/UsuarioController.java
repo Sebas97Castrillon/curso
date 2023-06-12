33a7224853bd3a4af4582fa364306abc46d7cb1f
Sebastian <sebas.2561@hotmail.com>
2023-06-12 17:37:35 -0500

    Se añade metodo request a UsuarioController.java

--- curso/src/main/java/com/cursojava/curso/controller/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controller/UsuarioController.java
@@ -11,5 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RestController
 public class UsuarioController {
     
-    
+    @RequestMapping(value="prueba", method=RequestMethod.GET)
+    public List<String> prueba() {
+        return List.of("Manzana", "Kiwi", "Banana");
+    }
 }
