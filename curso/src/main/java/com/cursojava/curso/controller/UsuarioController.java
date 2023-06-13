234fccbea8a7b27bb0cecd0cc7d87e59cbe19970
Sebastian <sebas.2561@hotmail.com>
2023-06-13 13:34:42 -0500

    Se añade models/usuario y se modifica usuariocontroller

--- curso/src/main/java/com/cursojava/curso/controller/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controller/UsuarioController.java
@@ -2,8 +2,9 @@ package com.cursojava.curso.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.cursojava.curso.models.Usuario;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,8 +12,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RestController
 public class UsuarioController {
     
-    @RequestMapping(value="prueba", method=RequestMethod.GET)
-    public List<String> prueba() {
-        return List.of("Manzana", "Kiwi", "Banana");
+    @RequestMapping(value="usuario/{id}", method=RequestMethod.GET)
+    public Usuario obtenerUsuario(@PathVariable String id) {
+        Usuario usuario = new Usuario();
+        usuario.setId(1);
+        usuario.setNombre("Juan");
+        usuario.setApellido("Perez");
+        usuario.setEmail("kenaa@example.com");
+        usuario.setTelefono("123456789");
+        usuario.setPassword("123456");
+        return usuario;
     }
+
+    // Crud usuario
+    @RequestMapping(value="usuario", method=RequestMethod.POST)
+    public Usuario crearUsuario(Usuario usuario) {
+        return usuario;
+    }
+    
+    @RequestMapping(value="usuario/{id}", method=RequestMethod.PUT)
+    public Usuario actualizarUsuario(@PathVariable String id, Usuario usuario) {
+        return usuario;
+    }
+    
+    @RequestMapping(value="usuario/{id}", method=RequestMethod.DELETE)
+    public void eliminarUsuario(@PathVariable String id) {
+        
+    }
+    
 }
