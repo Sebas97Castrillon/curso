3eac72f1b1dcc8a69dfe7d5e554a349cbbccb069
Sebastian <sebas.2561@hotmail.com>
2023-06-14 15:32:09 -0500

    "Se adelante en la funcionalidad listar usuarios"

--- curso/src/main/java/com/cursojava/curso/controller/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controller/UsuarioController.java
@@ -4,15 +4,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cursojava.curso.models.Usuario;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @RestController
 public class UsuarioController {
-    
-    @RequestMapping(value="usuario/{id}", method=RequestMethod.GET)
+
+    @RequestMapping(value = "usuario/{id}", method = RequestMethod.GET)
     public Usuario obtenerUsuario(@PathVariable String id) {
         Usuario usuario = new Usuario();
         usuario.setId(1);
@@ -24,20 +26,39 @@ public class UsuarioController {
         return usuario;
     }
 
-    // Crud usuario
-    @RequestMapping(value="usuario", method=RequestMethod.POST)
-    public Usuario crearUsuario(Usuario usuario) {
-        return usuario;
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> mostrar() {
+        List<Usuario> usuarios = new ArrayList<>();
+
+        Usuario usuario = new Usuario();
+        usuario.setId(1);
+        usuario.setNombre("Juan");
+        usuario.setApellido("Perfasfez");
+        usuario.setEmail("kenaa@example.com");
+        usuario.setTelefono("123456789");
+        usuario.setPassword("123456");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(2);
+        usuario2.setNombre("Juan");
+        usuario2.setApellido("Perez");
+        usuario2.setEmail("kenaa@example.com");
+        usuario2.setTelefono("123456789");
+        usuario2.setPassword("123456");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(3);
+        usuario3.setNombre("saf");
+        usuario3.setApellido("Pefasfrez");
+        usuario3.setEmail("kenaa@example.com");
+        usuario3.setTelefono("123456789");
+        usuario3.setPassword("123456");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+
+        return usuarios;
     }
-    
-    @RequestMapping(value="usuario/{id}", method=RequestMethod.PUT)
-    public Usuario actualizarUsuario(@PathVariable String id, Usuario usuario) {
-        return usuario;
-    }
-    
-    @RequestMapping(value="usuario/{id}", method=RequestMethod.DELETE)
-    public void eliminarUsuario(@PathVariable String id) {
-        
-    }
-    
+
 }
--- curso/src/main/resources/static/js/usuarios.js
+++ curso/src/main/resources/static/js/usuarios.js
@@ -1,6 +1,21 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function () {
-
-  alert(12345)
+  cargarUsuarios();
   $('#usuarios').DataTable();
 });
+
+async function cargarUsuarios() {
+
+  const request = await fetch('usuarios', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  });
+  const usuarios = await request.json();
+  document.querySelector('#usuarios tbody').outerHTML = ""
+
+  console.log(usuarios);
+
+}
